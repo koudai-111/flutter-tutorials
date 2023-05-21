9d02f90ac2e82f56ad26025e7137012f5e3dccbd
Koudai Yamane <koudai.hair.111@gmail.com>
2023-05-21 14:02:05 +0900

    flutterのチュートリアル実装

--- playground.dart
+++ playground.dart
@@ -32,13 +32,49 @@ import 'dart:io';
 //(?.), (??), (??=)
 
 // colletion
-main() {
-  // List
-  List names = ['Jack', 'Jill'];
-  print(names[0]);
-  print(names.length);
+// main() {
+//   // List
+//   List names = ['Jack', 'Jill'];
+//   print(names[0]);
+//   print(names.length);
 
-  // Set
-  Set<int> numbers = {1, 2, 3, 4,1};
-  print(numbers);
+//   // Set
+//   Set<int> numbers = {1, 2, 3, 4,1};
+//   print(numbers);
+// }
+
+// 引数にデフォルトを決めておく
+// hoge(var name, [var age = 18]);
+
+//class 
+class Person {
+  String name = 'Mahmud';
+  int age = 18;
+
+// constructor
+  Person(String name, [int age = 18]) {
+    this.name = name;
+    this.age = age;
+  }
+  // named constructor
+  Person.guest() {
+    name = 'Guest';
+    age = 18;
+  }
+
+  void showOutput() {
+    print(name);
+    print(age);
+  }
+}
+
+void main() {
+  Person person1 = Person('Jack', 23);
+  person1.showOutput();
+
+  var person2 = Person('Jill');
+  person2.showOutput();
+
+  var person3 = Person.guest();
+  person3.showOutput();
 }
