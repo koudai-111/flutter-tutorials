9747abb364313284e96493c315c34e362924967d
Koudai Yamane <koudai.hair.111@gmail.com>
2023-05-21 12:06:49 +0900

    first commit

---
+++ playground.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+// void main(){
+//   var firstName = "Mahmud";
+//   String lastName = "Ahsan";
+//   print(firstName + " " + lastName);
+// }
+
+
+// ターミナルで入力したものを表示する。
+// main() {
+//   //This program asks a user to enter their name and then prints it.
+//   stdout.writeln("What is your name: ");
+//   String? n = stdin.readLineSync();
+//   print('My name is $n');
+// }
+
+// In-Line comment
+
+/*
+  Block comment
+*/
+
+/// Documentation comment
+
+
+// relational ==, !=, >=, <=
+// && || and or
+// !=
+
+//Null Aware Operator
+//(?.), (??), (??=)
+
+// colletion
+main() {
+  // List
+  List names = ['Jack', 'Jill'];
+  print(names[0]);
+  print(names.length);
+
+  // Set
+  Set<int> numbers = {1, 2, 3, 4,1};
+  print(numbers);
+}
